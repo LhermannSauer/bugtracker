56e2eb85198505967be97e552b209b74c0090746
Leandro <lihermann@outlook.com>
2023-07-08 11:48:56 -0300

    Extract typeorm configuration to its own file

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,33 @@
-import express from 'express'
-import path from "path"
+import express from "express";
+import path from "path";
+import "dotenv/config";
 
-import {router as indexRouter} from './routes/index' 
-import {router as bugsRouter} from './routes/bugs'
+import { router as indexRouter } from "./routes/index";
+import { router as bugsRouter } from "./routes/bugs";
+import { AppDataSource } from "./typeorm.config";
 
 const app = express();
 
+AppDataSource.initialize()
+  .then(async (c) => {
+    await c.query(`
+                    USE bugtracker_dev;
+                    `);
+
+    console.log("connected to db");
+  })
+  .catch((e) => console.log(e));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/bugs', bugsRouter)
+app.use("/", indexRouter);
+app.use("/bugs", bugsRouter);
 
-const server = app.listen(3000,() =>{
-    console.log("Listening at port 3000...")
-})
+const port = process.env.PORT ?? 3000;
+const server = app.listen(port, () => {
+  console.log(`Listening at port ${port}...`);
+});
 
 module.exports = server;
-
-
---
+++ src/typeorm.config.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import "dotenv/config";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  synchronize: true,
+  entities: ["src/entities/*"],
+});
